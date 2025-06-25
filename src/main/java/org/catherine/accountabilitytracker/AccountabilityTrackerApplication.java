5ae536c93e1ec0ebcf55b9849b6decba319c4715
catherinecc <catherinechristyc@gmail.com>
2025-06-25 15:55:00 +0530

    Initial commit

---
+++ src/main/java/org/catherine/accountabilitytracker/AccountabilityTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.catherine.accountabilitytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountabilityTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountabilityTrackerApplication.class, args);
+    }
+
+}
