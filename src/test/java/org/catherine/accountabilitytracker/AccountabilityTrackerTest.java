99062ddf0aeeb581a03c114d22a759f7f716d092
catherinecc <catherinechristyc@gmail.com>
2025-07-10 13:27:06 +0530

    fixing testcase tests

--- src/test/java/org/catherine/accountabilitytracker/AccountabilityTrackerTest.java
+++ src/test/java/org/catherine/accountabilitytracker/AccountabilityTrackerTest.java
@@ -1,21 +1,29 @@
 package org.catherine.accountabilitytracker;
 
 import org.catherine.accountabilitytracker.Controllers.AccountabilityController;
+import org.catherine.accountabilitytracker.security.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(AccountabilityController.class)
+@WebMvcTest(AccountabilityController.class) // Controller class to be tested
+@Import(SecurityConfig.class) // Import the SecurityConfig to apply security configurations during tests
+
 public class AccountabilityTrackerTest {
     @Autowired
     MockMvc mockMvc;
+
+    @MockBean
+    private UserDetailsService userDetailsService;
 
     @Test
     public void testAddGoal() throws Exception {
